f8e1a276f71ed6fcab4b6ff070339a8cf2e30b61
leo0961325 <leo09613257@gmail.com>
2022-09-17 17:28:41 +0800

    Json getNames example

--- src/test/java/com/example/websocket/WebsocketApplicationTests.java
+++ src/test/java/com/example/websocket/WebsocketApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.websocket;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WebsocketApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JSONException {
+
+        String example = "{\n" +
+                "\t\"id\": \"0001\",\n" +
+                "\t\"type\": \"donut\",\n" +
+                "\t\"name\": \"Cake\",\n" +
+                "\t\"image\":\n" +
+                "\t\t{\n" +
+                "\t\t\t\"url\": \"images/0001.jpg\",\n" +
+                "\t\t\t\"width\": 200,\n" +
+                "\t\t\t\"height\": 200\n" +
+                "\t\t},\n" +
+                "\t\"thumbnail\":\n" +
+                "\t\t{\n" +
+                "\t\t\t\"url\": \"images/thumbnails/0001.jpg\",\n" +
+                "\t\t\t\"width\": 32,\n" +
+                "\t\t\t\"height\": 32\n" +
+                "\t\t}\n" +
+                "}";
+
+        JSONObject jsonObject = new JSONObject(example);
+        String[] strings = JSONObject.getNames(jsonObject);
+        for(String key : strings){
+            System.out.println(key);
+        }
+
+
     }
 
 }
